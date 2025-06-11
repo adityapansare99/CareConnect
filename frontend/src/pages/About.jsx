ff0e35b216e5965d2307376c9d2bd80745cac370
Aditya <pansareaditya281@gmail.com>
2025-06-11 23:31:08 +0530

    Responsive web completed

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -46,16 +46,16 @@ const About = () => {
 
       <div className="flex flex-col md:flex-row mb-20">
         <div className="border md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-[#5f6FFF] hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
-          <b>EFFICIENCY:</b>
-          <p>Streamlined appointment scheduling that fits into your busy lifestyle.</p>
+          <b className="sm:mx-0 mx-4">EFFICIENCY:</b>
+          <p className="sm:mx-0 mx-4">Streamlined appointment scheduling that fits into your busy lifestyle.</p>
         </div>
         <div className="border md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-[#5f6FFF] hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
-          <b>CONVENIENCE:</b>
-          <p>Access to a network of trusted healthcare professionals in your area.</p>
+          <b className="sm:mx-0 mx-4">CONVENIENCE:</b>
+          <p className="sm:mx-0 mx-4">Access to a network of trusted healthcare professionals in your area.</p>
         </div>
         <div className="border md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-[#5f6FFF] hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
-          <b>PERSONALIZATION:</b>
-          <p>Tailored recommendations and reminders to help you stay on top of your health.</p>
+          <b className="sm:mx-0 mx-4">PERSONALIZATION:</b>
+          <p className="sm:mx-0 mx-4">Tailored recommendations and reminders to help you stay on top of your health.</p>
         </div>
       </div>
     </div>
--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -7,6 +7,7 @@ const Doctors = () => {
   const [ filterDoc, setfilterDoc ] = useState([]);
   const navigate = useNavigate();
   const { doctors } = useContext(AppContext);
+  const [filter,setfilter]=useState(false);
 
   const applyfilter=()=>{
     if(speciality){
@@ -26,7 +27,10 @@ const Doctors = () => {
     <div>
       <p className="text-gray-600 ">Browse through the doctors specialist.</p>
       <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
-        <div className="flex flex-col gap-4 text-sm text-gray-600">
+        <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${filter?"bg-[#5f6FFF] text-white": ""}`} onClick={()=>{
+          setfilter(prev=>!prev);
+        }}>Filters</button>
+        <div className={`flex-col gap-4 text-sm text-gray-600 ${filter?"flex":"hidden sm:flex"}`}>
           <p onClick={()=>{
             speciality==='General physician'?navigate('/doctors'):navigate('/doctors/General physician')
           }} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality==="General physician"? "bg-gray-100 text-black" : ""}`}>General physician</p>
